f5aeb72f4f408c4b675df69a6d84e4400728ef51
wuqian <wuqianqian@eqxiu.com>
2022-03-16 18:15:14 +0800

    初次提交

--- .vuepress/siderbar.js
+++ .vuepress/siderbar.js
@@ -1,7 +1,18 @@
 const { createSideBarConfig } = require('./util')
-const DESIGN_PATTERN = '/blogs/design-pattern'
+
+const WEB_ENGINEERING_PATH = '/blogs/engineering'
+const NETWORK_PATH = '/blogs/network'
+const DESIGN_PATTERN_PATH = '/blogs/design-pattern'
+
 module.exports = {
-  [DESIGN_PATTERN]: [
-    createSideBarConfig('设计模式', DESIGN_PATTERN),
+  [WEB_ENGINEERING_PATH]: [
+    createSideBarConfig('前端工程化', WEB_ENGINEERING_PATH),
+  ],
+  [NETWORK_PATH]: [
+    createSideBarConfig('计算机网络', NETWORK_PATH),
+    createSideBarConfig('TCP/IP', NETWORK_PATH + '/TCP-IP'),
+  ],
+  [DESIGN_PATTERN_PATH]: [
+    createSideBarConfig('设计模式', DESIGN_PATTERN_PATH),
   ],
 }
